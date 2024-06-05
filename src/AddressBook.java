dccbcae422f0114abe9ec069f36ab92ede8a95af
Dovu23 <carloscarmona4554@gmail.com>
2024-06-04 19:30:22 -0600

    Agregando un metodo 'loadFromFile' en addressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -11,24 +14,73 @@ public class AddressBook {
     }
 
     public void addAddress(AddressEntry entry) {
-        entries.add(entry);
-        Collections.sort(entries, Comparator.comparing(AddressEntry::getApellido));
+        if (!entryExists(entry)) {
+            entries.add(entry);
+            Collections.sort(entries, Comparator.comparing(AddressEntry::getApellido));
+            System.out.println("Contacto agregado: " + entry);
+        } else {
+            System.out.println("El contacto ya existe: " + entry);
+        }
     }
 
     public void removeAddress(AddressEntry entry) {
-        entries.remove(entry);
+        if (entries.remove(entry)) {
+            System.out.println("Contacto eliminado: " + entry);
+        } else {
+            System.out.println("El contacto no existe: " + entry);
+        }
     }
 
     public List<AddressEntry> findApellido(String apellido) {
         List<AddressEntry> result = new ArrayList<>();
         for (AddressEntry entry : entries) {
-            if (entry.getApellido().equals(apellido)) {
+            if (entry.getApellido().toLowerCase().startsWith(apellido.toLowerCase())) {
                 result.add(entry);
             }
         }
         return result;
     }
+
     public List<AddressEntry> getAllEntries() {
         return new ArrayList<>(entries);
+    }
+
+    public void loadFromFile(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(" ");
+                if (data.length == 8) {
+                    AddressEntry newEntry = new AddressEntry(
+                            data[0], //Nombre
+                            data[1], //Apellido
+                            data[2], //Calle
+                            data[3], //Ciudad
+                            data[4], //Estado
+                            data[5], //CodigoPostal
+                            data[6], //Email
+                            data[7]  //Telefonoadd
+                    );
+                    if (!entryExists(newEntry)) {
+                        addAddress(newEntry);
+                    } else {
+                        System.out.println("El contacto " + newEntry + " ya existe y no se agregara nuevamente.");
+                    }
+                } else {
+                    System.out.println("Linea mal formada: " + line);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+
+    private boolean entryExists(AddressEntry newEntry) {
+        for (AddressEntry entry : entries) {
+            if (entry.equals(newEntry)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
